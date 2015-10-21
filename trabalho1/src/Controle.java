d414d52f2283e13956bf29b983491f83e952f440
Mariany ferreira da Silva <marianyufsc@gmail.com>
2015-10-21 16:38:32 -0200

    Update Controle.java
    Correções na instanciação da Matriz.

--- trabalho1/src/Controle.java
+++ trabalho1/src/Controle.java
@@ -16,9 +16,29 @@ public class Controle {
 	public static void main(String[] args) {
 		// Declaração da Interface como Objeto
 		Interface aInterface = new Interface();
+		
 		// criação e inicialização de uma matriz (conforme o problema)
-		int matriz[][] = { { 1, 1, 1, 0 }, { 0, 1, 1, 0 }, { 1, 0, 1, 0 }, { 0, 0, 1, 0}, };
-		//Declaração de Rotas como Objeto e atribuição da matriz para o construtor do objeto
+		//int matriz[][] = { { 1, 1, 1, 0 }, { 0, 1, 1, 0 }, { 1, 0, 1, 0 }, { 0, 0, 1, 0}, };
+		
+		//criação de muma matriz genérica e concatenação da matriz para mostrar a Matriz montada
+		int ordem = aInterface.getInteger("Digite a ordem da Matriz");
+		int matriz[][] = new int [ordem][ordem];
+		
+		String concatenaMatriz = "";
+		
+		for (int linha = 0; linha < matriz.length; linha++){
+			
+			for (int coluna = 0; coluna < matriz[0].length; coluna++){
+				
+				matriz[linha][coluna] = aInterface.getInteger("Informe o valor a ser armazenado (Linha: [ " + (linha) + " ] " + " Coluna: [ " + (coluna) + " ])");
+				concatenaMatriz += " [ " +  matriz[linha][coluna] + " ] "; 
+			}
+			
+			concatenaMatriz += "\n";
+		}
+		
+		
+		//Declaração de Rotas como Objeto e atribuição da matriz para o construtor do objeto	
 		Rotas aRotas = new Rotas(matriz);
 		//Pedido da Constante correspondente a cidade de pesquisa
 		int k = aInterface.getInteger("Informe a Cidade para Procura");
@@ -43,13 +63,15 @@ public class Controle {
 		
 		
 		// Resultados
-		aInterface.showMessage("1) A cidade " + k + " possui " + (saidas) + " saidas " + " e " + chegadas + " chegadas.\n\n" +
+		aInterface.showMessage("Matriz: \n"
+								+ concatenaMatriz + "\n" +
+							   "1) A cidade " + k + " possui " + (saidas) + " saidas " + " e " + chegadas + " chegadas.\n\n" +
 		
 							   "2) A cidade " + maiorNChegadas + " possui o maior numero de chegadas\n\n" +
 							   
 							   "3) A cidade " + k + (ligacoesDiretas ? " possui" : " nao possui") + " todas as estradas de mao dupla\n\n" +
 							   
-							   "4) A cidade " + saidasDiretasK  +  " possue saida direta para a cidade " + k + "\n\n" +
+							   "4) A cidade:" + saidasDiretasK  +  ". Possue saida direta para a cidade " + k + "\n\n" +
 							   
 							   "5) \n" +
 							   " - As cidades Isoladas sao: " + cidadesIsoladas + "\n" +
